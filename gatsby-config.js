849ff756a9343e5a1d649c4f80628cbeb05f309c
ttbarnes <tony@tonybarnes.me>
2022-10-17 18:25:12 +0100

    remove img filesytem config

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,37 +21,13 @@ module.exports = {
         name: "pages",
       },
     },
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        path: `${__dirname}/src/img`,
-        name: "images",
-      },
-    },
     `gatsby-plugin-image`,
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
       resolve: "gatsby-transformer-remark",
       options: {
-        plugins: [
-          'gatsby-remark-relative-images',
-          {
-            resolve: "gatsby-remark-images",
-            options: {
-              // It's important to specify the maxWidth (in pixels) of
-              // the content container as this plugin uses this as the
-              // base for generating different widths of each image.
-              maxWidth: 2048,
-            },
-          },
-          {
-            resolve: "gatsby-remark-copy-linked-files",
-            options: {
-              destinationDir: "static",
-            },
-          },
-        ],
+        plugins: [],
       },
     },
     {
