db4b9547400c3eff7ba221eaf436b3e054c7e656
ttbarnes <tony@tonybarnes.me>
2022-11-06 18:08:06 +0000

    add viewport tag

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -12,6 +12,7 @@ const TemplateWrapper = ({ children }) => {
         <html lang="en" />
         <title>{title}</title>
         <meta name="description" content={description} />
+        <meta name="viewport" content="height=device-height,width=device-width,initial-scale=1.0" />
 
         <link
           rel="apple-touch-icon"
